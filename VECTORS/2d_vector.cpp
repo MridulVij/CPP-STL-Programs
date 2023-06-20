a0888734bf971bb52fce7089c6ef9ae9a4189bf6
mridul <mridulvij1569@gmail.com>
2023-06-20 23:15:21 +0530

    changes

--- VECTORS/2d_vector.cpp
+++ VECTORS/2d_vector.cpp
@@ -5,6 +5,26 @@ int main()
 {
 vector<vector<int>> v;
 
+//values inserted already / manually
+//v ={{10,20,30,40},{20,40,50,50},{90,50,20,20}};
+
+
+
+// for inserting values in 2d matrix
+for (int i=0; i<=v.size(); i++){
+    for(int j=0; j<=v.size(); j++){
+        
+    }
+}
+
+// for printing 2d-matrix completly
+for(int i=0; i<=v.size(); i++){
+    for(int j=0;  j<=v.size(); j++){
+        cout<<v[i][j]<<" ";
+    }
+    cout<<endl;
+}
+
 
 return 0;
 }
