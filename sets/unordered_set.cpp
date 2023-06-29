dddd0d48ffdc9eb4d013c3880e87c72f09fee92d
mridul <mridulvij1569@gmail.com>
2023-06-29 23:11:28 +0530

    fixxes

--- sets/unordered_set.cpp
+++ sets/unordered_set.cpp
@@ -18,11 +18,26 @@ int main()
     //     cout<<(*it);
     // }
 
-    if(ump.find(50)==ump.end()){
-        cout<<"Element Found";
-    }else{
-        cout<<"Not Found";
-    }
+    // if(ump.find(60)!=ump.end()){
+    //     cout<<"Element Found";
+    // }else{
+    //     cout<<"Not Found";
+    // }
+
+    // if(ump.count(50))
+    //     cout<<"Found"<<ump.count(50);
+    // else 
+    //     cout<<"Not Found";
+
+    // cout<<ump.size();
+    // ump.erase(10);
+    // ump.erase(ump.begin(), ump.end());
+    // cout<<ump.size();
+
+    
+
+    // TC - O(1)
+    // SC - O(1)
 
 return 0;
 }
