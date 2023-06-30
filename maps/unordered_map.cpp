3cf13e4b7b37e2d02177f089758daba5f53a5dd2
mridul <mridulvij1569@gmail.com>
2023-06-30 19:59:36 +0530

    added code

--- maps/unordered_map.cpp
+++ maps/unordered_map.cpp
@@ -14,9 +14,38 @@ int main()
     ump.insert({"hello", 20});
     // functions
 
-    for(auto x:ump)
-        cout<<x.first<<" "<<x.second<<endl;
+    // for(auto x:ump)
+    //     cout<<x.first<<" "<<x.second<<endl;
     
+
+    // if(ump.find("hello")!=ump.end())
+    //  cout<<"Found";
+     
+    // else 
+    //  cout<<"Not Found";
+
+    // auto it = ump.find("hello");
+    // if(it != ump.end()){
+    //     cout<<(it->second);
+    // }
+
+
+    for(auto it = ump.begin(); it!=ump.end(); it++){
+        cout<<it->second;
+                
+    }
+
+    if(ump.count("hello")>0){
+        cout<<"Found";
+    }else{
+        cout<<"Not Found";
+    }
+
+    //erase()  ump.erase(), ump.erase(ump.begin(), ump,end());
+    //size()
+
+// TC - O(1)
+// SC - O(1)
 
 
 return 0;
